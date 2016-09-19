8a5fa885826f3f225c9983d9df7d36a64882a5f4
Paul Salden <paul.salden@gmail.com>
2016-09-19 14:13:14 +0200

    update to newer plotcompliance.py

--- plotcompliances.py
+++ plotcompliances.py
@@ -6,7 +6,8 @@ pgf_with_rc_fonts = {
     "font.family": "Arial",
     "font.serif": [],
     "font.sans-serif": ["Arial"],
-    "font.size": 10,
+    "font.size": 8,
+    "figure.dpi": 600,
     "axes.prop_cycle": (cycler('color', [
             (0, 166./255, 214./255),
             (165./255, 202./255, 26./255),
@@ -16,17 +17,25 @@ pgf_with_rc_fonts = {
 }
 matplotlib.rcParams.update(pgf_with_rc_fonts)
 
-name = sys.argv[1]
+infile = sys.argv[1]
+outfile = sys.argv[2]
 
-p = pickle.load(open("results/{}-comp.p".format(name), "rb"))
+p = pickle.load(open(infile, "rb"))
 
-pl.figure(figsize=(4, 2))
+pl.figure(figsize=(3, 1.5))
 
-pl.plot(p["compliances"])
+if len(sys.argv) == 4 and sys.argv[3] == "z":
+  start = 400
+  comp = p["compliances"][start:]
+  x = range(start, start + len(comp))
+
+  pl.plot(x, comp)
+else:
+  pl.plot(p["compliances"])
 
 ax = pl.gca()
 
 ax.set_xlabel('iteration')
 ax.set_ylabel('compliance')
 
-pl.savefig("results/{}-compliances.pdf".format(name), bbox_inches="tight")
+pl.savefig(outfile, bbox_inches="tight", dpi="figure")
